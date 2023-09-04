2cdecd55acd7e65fe46c90930fac28af834109c9
Miguel Vega <miguel.vega@gmail.com>
2023-09-04 12:45:30 +0200

    feat(first-project-00-hola-mundo)

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <button>1</button>
+    <button>2</button>
+  </>
+  )
