b943be6f96ab6296c105d85a0021841dac26fcf6
Miguel Vega <miguel.vega@gmail.com>
2023-11-08 22:51:07 +0100

    feat(04-react-prueba-tecnica): fetching api

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT =`https://catfact.ninja/fact`
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
+
 export function App () {
+    const [fact, setFact] = useState()
+    const [imageUrl, setImageUrl] = useState()
+
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => {
+            const { fact } = data
+            setFact(fact)
+
+            const firstWord = fact.split(' ', 1).join(' ')
+            console.log(firstWord)
+
+            fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+            // fetch(`https://cataas.com/cat?json=true`)
+                .then(res => res.json())
+                .then(response => {
+                    console.log(response)
+                    const { _id } = response
+                    setImageUrl(`https://cataas.com/cat/says/${firstWord}`)
+                })
+
+        })
+    }, [])
+
     return (
-        <h1>App de gatitos</h1>
+        <main>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={imageUrl} alt={`${fact}`} />}
+        </main>
     )
 }
